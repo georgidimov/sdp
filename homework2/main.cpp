958ed36e2af1af0a3f2291e416f82181574a49ce
Georgi Dimov <gt_dimov@mail.bg>
2014-11-13 20:12:32 +0200

    Add class Node with getters and setters.

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -1,8 +1,8 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <node.h>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    
-    return a.exec();
+int main(){
+
+    return 0;
 }
