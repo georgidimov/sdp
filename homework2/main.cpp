bc1ffbe13a34c323adac73056fa467222854b600
Georgi Dimov <gt_dimov@mail.bg>
2014-11-15 22:12:00 +0200

    Back to working with one cash.

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -1,16 +1,46 @@
 #include <QCoreApplication>
 #include <iostream>
 #include <queue.hpp>
-#include <node.hpp>
-
+#include <market.h>
 int main(){
-    size_t queuesSize = 5;
-    Queue<int> * queues = new Queue<int> [queuesSize];
+    Market market(5);
+    size_t gubiesCount = 12;
+    Client * gubies = new Client[gubiesCount];
+    for(size_t i = 0; i < gubiesCount - 1; ++i){
+        gubies[i].creditCard = false;
+        gubies[i].ID = -1;
+        gubies[i].numberOfGoods = 5;
+    }
 
-    delete [] queues;
-    //q.split();
+    gubies[gubiesCount - 1].numberOfGoods = 2;
+    gubies[gubiesCount - 2].numberOfGoods = 2;
+    gubies[gubiesCount - 3].numberOfGoods = 2;
 
+    market.AddClient(gubies, gubiesCount);
 
-    //std :: cout << std :: endl << q.begin() << "  " << q.end() << std :: endl
-    //              << q.begin() == q.end();
+    /*
+    Queue<int> * qs = new Queue<int> [5];
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < i + 1; ++j){
+            qs[i].enqueue(j);
+        }
+    }
+
+    int shortestQueueIndex = 0;
+    size_t clientsCount = 0;
+
+    for(int i = 0; i < 5; ++i){
+        if(qs[i].getSize() < clientsCount){
+            clientsCount = qs[i].getSize();
+            shortestQueueIndex = i;
+        }
+    }
+
+    qs[1].dequeue();
+    std :: cout << qs[0].getSize() << " " << qs[1].getSize() << std :: endl;
+
+    std :: cout << shortestQueueIndex;
+
+    delete [] qs;
+    */
 }
