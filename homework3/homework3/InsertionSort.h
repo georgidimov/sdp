49b68f5d6ca43b56e609b4b934985751739d132e
Georgi Dimov <gt_dimov@mail.bg>
2014-12-11 19:15:17 +0200

    Add sort method and works fine.

--- homework3/homework3/InsertionSort.h
+++ homework3/homework3/InsertionSort.h
@@ -4,7 +4,46 @@
 
 template <class T>
 class InsertionSort : public Sorter<T>{
+private:
+    void swap(T & a, T & b) const;
 public:
     InsertionSort();
+
     virtual void sort(T *data, size_t count);
+    virtual unsigned long long getSortTime() const;
 };
+
+template <class T>
+InsertionSort<T> :: InsertionSort(){
+;
+}
+
+template <class T>
+void InsertionSort<T> :: sort(T * data, size_t count){
+    T tempValue;
+    size_t tempPosition;
+
+    for(size_t i = 1; i < count; ++i){
+        tempValue = data[i];
+        tempPosition = i;
+        while(tempPosition > 0 && tempValue < data[tempPosition - 1]){
+            data[tempPosition] = data[tempPosition - 1];
+            --tempPosition;
+        }
+
+        data[tempPosition] = tempValue;
+    }
+}
+
+
+template <class T>
+void InsertionSort<T> :: swap(T & a, T & b) const{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <class T>
+unsigned long long InsertionSort<T> :: getSortTime() const{
+    return 0;
+}
