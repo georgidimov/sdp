47006d87cea1d9acfbec1c8ce81351fd9d7d2a3d
Georgi Dimov <gt_dimov@mail.bg>
2014-12-11 19:14:46 +0200

    Add methods to SortTester.

--- homework3/homework3/SortTester.h
+++ homework3/homework3/SortTester.h
@@ -8,3 +8,10 @@ public:
     SortTester(Sorter<T> ** sorters, int count);
     void getSummary(std::ostream & out);
 };
+
+
+template <class T>
+SortTester<T> :: SortTester(Sorter<T> ** sorters, int count){
+    ;
+}
+
