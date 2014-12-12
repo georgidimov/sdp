b1b7629c336977ef2ce1eb03d07ec1c55da67fa9
Georgi Dimov <gt_dimov@mail.bg>
2014-12-12 19:18:25 +0200

    Syntax fixes in InsertionSort.

--- homework3/homework3/InsertionSort.hpp
+++ homework3/homework3/InsertionSort.hpp
@@ -1,19 +1,19 @@
 #pragma once
 #include <iostream>
-#include <Sorter.h>
+#include <Sorter.hpp>
 
 template <class T>
 class InsertionSort : public Sorter<T>{
 public:
     InsertionSort();
 
-    virtual void sort(T *data, size_t count);
+    virtual void sort(T * data, size_t count);
     virtual unsigned long long getSortTime() const;
 };
 
 template <class T>
 InsertionSort<T> :: InsertionSort(){
-;
+    ;
 }
 
 template <class T>
@@ -24,6 +24,7 @@ void InsertionSort<T> :: sort(T * data, size_t count){
     for(size_t i = 1; i < count; ++i){
         tempValue = data[i];
         tempPosition = i;
+
         while(tempPosition > 0 && tempValue < data[tempPosition - 1]){
             data[tempPosition] = data[tempPosition - 1];
             --tempPosition;
