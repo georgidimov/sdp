707a12a0c776dcef5120f0f809b735ec1e8f2c12
Georgi Dimov <gt_dimov@mail.bg>
2014-12-13 20:59:00 +0200

    Add implementation to siftDown method

--- homework3/homework3/HeapSort.hpp
+++ homework3/homework3/HeapSort.hpp
@@ -4,7 +4,8 @@
 
 template <class T>
 class HeapSort : public Sorter<T>{
-    void siftDown(T * array, size_T startPosition, size_t endPosition) const;
+    void swap(T & a, T & b) const;
+    void siftDown(T * array, size_t startPosition, size_t size) const;
 public:
     HeapSort();
 
@@ -18,8 +19,40 @@ HeapSort<T> :: HeapSort(){
 }
 
 template <class T>
-void HeapSort<T> :: siftDown(T * array, size_T startPosition, size_t endPosition) const{
-    ;
+void HeapSort<T> :: swap(T & a, T & b) const{
+    T temp = a;
+    a = temp;
+    a = b;
+    b = temp;
+}
+
+template <class T>
+void HeapSort<T> :: siftDown(T * array, size_t startPosition, size_t size) const{
+    T valueAtTheBeginning = array[startPosition];
+
+    size_t childPosition = startPosition * 2 + 1;
+
+    while(childPosition < size){
+        //check rigth child if is with greater value get this position
+        if(array[childPosition] < array[childPosition + 1] && childPosition + 1 < size){
+            ++childPosition;
+        }
+
+        //check if the tree is ordered
+        if(valueAtTheBeginning > array[childPosition]){
+            return;
+        }
+
+        if(array[startPosition] < array[childPosition]){
+            swap(array[startPosition], array[childPosition]);
+        }
+
+        //jump to next node
+        startPosition *= 2;
+        startPosition += 1;
+
+        childPosition = startPosition * 2 + 1;
+    }
 }
 
 
