52211ee67d4b40f38cfea153289073d2c8ee553d
Georgi Dimov <gt_dimov@mail.bg>
2014-12-13 23:54:38 +0200

    Method sort in MergeSort work fine without many tests.

--- homework3/homework3/MergeSort.hpp
+++ homework3/homework3/MergeSort.hpp
@@ -4,17 +4,30 @@
 
 template <class T>
 class MergeSort : public Sorter<T>{
+    T * result;
+    size_t resultCounter;
+
     void swap(T & a, T & b) const;
+    void merge(T * a, size_t aSize, T * b, size_t bSize);
 public:
     MergeSort();
+    ~MergeSort();
 
     virtual void sort(T * array, size_t size);
     virtual unsigned long long getSortTime() const;
+
+    void print() const;
 };
 
 template <class T>
 MergeSort<T> :: MergeSort(){
-    ;
+    result = NULL;
+    resultCounter = 0;
+}
+
+template <class T>
+MergeSort<T> :: ~MergeSort(){
+    delete [] result;
 }
 
 template <class T>
@@ -26,11 +39,83 @@ void MergeSort<T> :: swap(T & a, T & b) const{
 }
 
 template <class T>
+void MergeSort<T> :: merge(T * a, size_t aSize, T * b, size_t bSize){
+    size_t aCounter = 0;
+    size_t bCounter = 0;
+
+    while(aCounter < aSize && bCounter < bSize) {
+        if(a[aCounter] <= b[bCounter]){
+            result[resultCounter] = a[aCounter];
+            ++aCounter;
+        }else{
+            result[resultCounter] = b[bCounter];
+            ++bCounter;
+        }
+
+        ++resultCounter;
+    }
+
+    while(aCounter < aSize){
+        result[resultCounter] = a[aCounter];
+
+        ++aCounter;
+        ++resultCounter;
+    }
+
+    while(bCounter < bSize){
+        result[resultCounter] = b[bCounter];
+
+        ++bCounter;
+        ++resultCounter;
+    }
+
+    for(size_t i = 0; i < resultCounter; ++i){
+        a[i] = result[i];
+    }
+
+    resultCounter = 0;
+}
+
+template <class T>
 void MergeSort<T> :: sort(T * array, size_t size){
+    for(size_t i = 0; i < size; ++i){
+       // std :: cout << array[i] << ' ';
+    }
+    std :: cout << std :: endl;
+    if(result == NULL){
+        result = new int[size];
+
+    }
+
+    if(size == 1){
+        //merge(array, 1, NULL, 0);
+        return;
+    }
+
+    if(size == 2){
+        if(array[0] > array[1]){
+            swap(array[0], array[1]);
+        }
+        //merge(array, 1, array + 1, 1);
+        return;
+    }
+
+    size_t newSize = size / 2;
+
+    sort(array, newSize);
+    sort(array + newSize, size - newSize);
+    merge(array, newSize, array + newSize, size - newSize);
 
 }
 
 template <class T>
 unsigned long long MergeSort<T> :: getSortTime() const{
     return 0;
+}
+
+template <class T>
+void MergeSort<T> :: print() const{
+    for(size_t i = 0; i < 8; ++i){
+        std :: cout << result[i] << ' ';
+    }
 }
