5d91c6763b77a0524ba747dcaefeddd7954dbe14
Georgi Dimov <gt_dimov@mail.bg>
2014-12-13 20:16:27 +0200

    Add methods from Sorter.

--- homework3/homework3/HeapSort.hpp
+++ homework3/homework3/HeapSort.hpp
@@ -2,3 +2,26 @@
 #include <Sorter.hpp>
 
 
+template <class T>
+class HeapSort : public Sorter<T>{
+public:
+    HeapSort();
+
+    virtual void sort(T * data, size_t count);
+    virtual unsigned long long getSortTime() const;
+};
+
+template <class T>
+HeapSort<T> :: HeapSort(){
+    ;
+}
+
+template <class T>
+void HeapSort<T> :: sort(T * data, size_t count){
+    ;
+}
+
+template <class T>
+unsigned long long HeapSort<T> :: getSortTime() const{
+    return 0;
+}
