6de88f7c1f4d9f43feefd7df278e7a857fd76cdf
Georgi Dimov <gt_dimov@mail.bg>
2014-12-29 14:58:17 +0200

    Add default root tag in XMLtree constructor

--- homework4/xmltree.cpp
+++ homework4/xmltree.cpp
@@ -1,5 +1,54 @@
 #include "xmltree.h"
 
 XMLtree :: XMLtree(){
-    root = NULL;
+    root = new Tag(NULL, Value((char *) "root"), Value((char *) ""));
+}
+
+XMLtree :: ~XMLtree(){
+    clear();
+}
+
+void XMLtree :: clear(){
+    ;
+}
+
+void XMLtree :: addTag(Value parent, Value k, Value v){
+
+}
+
+///Iterator`s part
+XMLtree :: Iterator :: Iterator(){
+    current = NULL;
+}
+
+XMLtree :: Iterator :: Iterator(Tag * k){
+    current = k;
+}
+
+XMLtree :: Iterator :: Iterator(Value k){
+    //write me
+}
+
+const XMLtree :: Iterator &  XMLtree :: begin(){
+    return Iterator(root);
+}
+
+const XMLtree :: Iterator &  XMLtree :: end(){
+    //write me
+}
+
+const XMLtree :: Iterator & XMLtree :: Iterator :: operator++(){
+    //write me
+}
+
+const XMLtree :: Iterator & XMLtree :: Iterator :: operator--(){
+    //write me
+}
+
+bool XMLtree :: Iterator :: operator ==(const XMLtree :: Iterator & rh) const{
+    return current == rh.current;
+}
+
+XMLtree :: Iterator :: operator bool() const{
+    return current == NULL;
 }
