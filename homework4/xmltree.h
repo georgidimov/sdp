6a0fe0a8794873e74266247cf94696d639dac05c
Georgi Dimov <gt_dimov@mail.bg>
2014-12-29 14:59:13 +0200

    Move iterator to protected access

--- homework4/xmltree.h
+++ homework4/xmltree.h
@@ -3,6 +3,32 @@
 
 class XMLtree{
     Tag * root;
+    void clear();
+//protected:
 public:
+    class Iterator{
+        Tag * current;
+    public:
+        Iterator();
+        Iterator(Tag * k);
+        Iterator(Value k);
+
+        const Iterator & operator++();
+
+        const Iterator & operator--();
+        bool operator ==(const Iterator & rh) const;
+
+        operator bool() const;
+    };
+
+//public:
     XMLtree();
+    ~XMLtree();
+    void addTag(Value parent, Value k, Value v);
+
+
+    const Iterator & begin();
+    const Iterator & end();
+
+
 };
