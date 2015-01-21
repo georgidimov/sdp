95efab0b3f3f540fe2f49d9bce16ee57a25f3a15
Georgi Dimov <gt_dimov@mail.bg>
2015-01-21 17:20:49 +0200

    Add method tick calculateWaterVolume, and isValidPosition.
    Some of them are without implementation.

--- homework5/streetgraph.cpp
+++ homework5/streetgraph.cpp
@@ -1,7 +1,7 @@
 #include "streetgraph.h"
 
 StreetGraph :: StreetGraph(std::ifstream & source){
-    streetWeight = NULL;
+    junctions = NULL;
     load(source);
 }
 
@@ -15,15 +15,105 @@ StreetGraph :: ~StreetGraph(){
 }
 
 void StreetGraph :: load(std::ifstream & source){
-    ///ADD IMPLENTATION
+    source.clear();
+    source.seekg(0);
+
+    //read matrix size
+    source >> n;
+    source >> m;
+    source >> waterPermeability;
+
+    //initialize needed matrix
+    initArray();
+    //std :: cout << n << ' ' << m << ' ' << waterPermeability <<  std :: endl;
+
+    //read matrix content
+    for(size_t i = 0; i < n; ++i){
+        for(size_t j = 0; j < m; ++j){
+            source >> junctions[i][j].height;
+            junctions[i][j].currentWaterVolume = waterPermeability;
+            junctions[i][j].newWaterVolume = waterPermeability;
+        }
+    }
+    tick();
+    //print();
+}
+
+void StreetGraph :: initArray(){
+    junctions = new Junction * [n];
+
+    for(size_t i = 0; i < n; ++i){
+        junctions[i] = new Junction [m];
+    }
+
+}
+
+int StreetGraph :: calculateWaterVolume(size_t i, size_t j) const{
+    int result = junctions[i][j].currentWaterVolume;
+
+    if(i == 0 || i == n - 1){   //is vertical border
+        //std :: cout << '(' << junctions[i][j].height << ", " << i << ", " << j << ") ";
+        result -= 2 * waterPermeability;
+    }
+
+    if(j == 0 || j == m - 1){   //is horizontal border
+        //std :: cout << '(' << junctions[i][j].height << ", " << i << ", " << j << ") ";
+        result -= 2 * waterPermeability;
+    }
+
+    i = 1;
+    j = 1;
+
+    int neighbourI, neighbourJ;
+
+    //left junction
+    neighbourI = i;
+    neighbourJ = j - 1;
+    std :: cout << junctions[neighbourI][neighbourJ].height << ' ';
+    //std :: cout << "(" << neighbourI << ", " << neighbourJ << ") ";
+    //down junction
+    neighbourI = i - 1;
+    neighbourJ = j;
+    //std :: cout << "(" << neighbourI << ", " << neighbourJ << ") ";
+std :: cout << junctions[neighbourI][neighbourJ].height << ' ';
+    //right junction
+    neighbourI = i;
+    neighbourJ = j + 1;
+    //std :: cout << "(" << neighbourI << ", " << neighbourJ << ") ";
+std :: cout << junctions[neighbourI][neighbourJ].height << ' ';
+    //up junction
+    neighbourI = i + 1;
+    neighbourJ = j;
+    //std :: cout << "(" << neighbourI << ", " << neighbourJ << ") ";
+std :: cout << junctions[neighbourI][neighbourJ].height << ' ';
+
+    return 0;
+}
+
+void StreetGraph :: tick(){
+    for(size_t i = 0; i < n; ++i){
+        for(size_t j = 0; j < m; ++j){
+            //std :: cout << "i = " << i << " j = " << j << std :: endl;
+            calculateWaterVolume(i, j);
+            return;
+        }
+    }
+}
+
+bool StreetGraph :: isValidPosition(int i, int j) const{
+    if(i >= 0 && i < n && j >= 0 && j < m){
+        return true;
+    }
+
+    return false;
 }
 
 void StreetGraph :: clear(){
-    for(int i = 0; i < n; ++i){
-        delete [] streetWeight[i];
+    for(size_t i = 0; i < n; ++i){
+        delete [] junctions[i];
     }
 
-    delete [] streetWeight;
+    delete [] junctions;
 
-    streetWeight = NULL;
+    junctions = NULL;
 }
--- homework5/streetgraph.h
+++ homework5/streetgraph.h
@@ -1,15 +1,40 @@
 #pragma once
+#include <iostream>
 #include <fstream>
 
-class StreetGraph{
-    int ** streetWeight;
-    int n, m;
+struct Junction{
+    int height;
+    int currentWaterVolume;
+    int newWaterVolume;
+};
 
-    void load(std :: ifstream & source);
-    void clear();
+class StreetGraph{
 public:
     StreetGraph(std :: ifstream & source);
     StreetGraph(const StreetGraph & rh);
     ~StreetGraph();
+
+private:
+    Junction ** junctions;
+    size_t n, m, waterPermeability;
+
+    void load(std :: ifstream & source);
+    void initArray();
+    void clear();
+
+    int calculateWaterVolume(size_t i, size_t j) const;
+    void tick();
+
+    bool isValidPosition(int i, int j) const;
+
+    //remove me
+    void print() const{
+        for(size_t i = 0; i < n; ++i){
+            for(size_t j = 0; j < m; ++j){
+                std :: cout << '('  << junctions[i][j].height << ", " <<  junctions[i][j].currentWaterVolume << "), ";
+            }
+            std :: cout << std :: endl;
+        }
+    }
 
 };
